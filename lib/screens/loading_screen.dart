2282faab44f5c3d9a509cbda18197a7656517b80
laxminarayan11 <laxminarayansharmanaresh@gmail.com>
2020-04-20 16:06:09 +0530

    positionfind

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +7,33 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
+
+
+  void getLocation() async {
+     try{
+      // somethingThatExpectslessThan10(1);
+       Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+       print(position);
+     } catch(e) {
+       print(e);
+     }
+  }
+
+  void somethingThatExpectslessThan10(int n) {
+    if(n > 10) {
+      throw 'n is greater thank 10, n should always be less than 10.';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
---
+++ lib/services/location.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+class Location {
+  double latitude;
+  double longitude;
+}
